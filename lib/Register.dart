0162f8bbb9dc3cac7b957b5e897170b051900c6c
Endairion <86313950+Endairion@users.noreply.github.com>
2023-05-08 23:54:40 +0800

    Finishing the UI of Register Page (No Logic)

--- lib/Register.dart
+++ lib/Register.dart
@@ -1,9 +1,18 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_app_development_cw2/common/theme_helper.dart';
 
-class Register extends StatelessWidget {
+import 'NavigationMenu.dart';
+
+class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
 
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  bool? isChecked1 = false, isChecked2 = false;
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -13,7 +22,7 @@ class Register extends StatelessWidget {
         child: Container(
           height: size.height,
           width: size.width,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             image: DecorationImage(
               image: AssetImage('assets/login_background.png'),
               fit: BoxFit.cover,
@@ -31,7 +40,7 @@ class Register extends StatelessWidget {
                     color: Colors.black.withOpacity(0.1),
                     spreadRadius: 5,
                     blurRadius: 7,
-                    offset: Offset(0,3),
+                    offset: const Offset(0,3),
                   ),
                 ],
               ),
@@ -50,7 +59,7 @@ class Register extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 6),
-                  Text(
+                  const Text(
                     "Register a new account",
                     style: TextStyle(
                       color: Colors.black,
@@ -59,7 +68,7 @@ class Register extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 8),
-                  Text(
+                  const Text(
                     "Fill in your personal details",
                     style: TextStyle(
                       color: Colors.black,
@@ -78,7 +87,7 @@ class Register extends StatelessWidget {
                   ),
                   const SizedBox(height: 8),
                   TextField(
-                    decoration: ThemeHelper().textInputDecoration("Date of Birth", "Date of Birth (DD/MM/YYYY"),
+                    decoration: ThemeHelper().textInputDecoration("Date of Birth", "Date of Birth (DD/MM/YYYY)"),
                     cursorColor: Colors.lightGreen,
                   ),
                   const SizedBox(height: 8),
@@ -103,6 +112,104 @@ class Register extends StatelessWidget {
                     decoration: ThemeHelper().textInputDecoration("Confirm Password", "Confirm Password"),
                     cursorColor: Colors.lightGreen,
                     obscureText: true,
+                  ),
+                  const SizedBox(height: 6),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: isChecked1,
+                        onChanged: (value) {
+                          setState(() {
+                            isChecked1 = value;
+                          });
+                        },
+                        checkColor: Colors.black,
+                        activeColor: Colors.grey[400],
+                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        visualDensity: VisualDensity.compact,
+                      ),
+                      Expanded(
+                        child: RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              color: Colors.black,
+                            ),
+                            children: [
+                              TextSpan(
+                                text: 'I agree to the ',
+                              ),
+                              TextSpan(
+                                text: 'terms and conditions',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  decoration: TextDecoration.underline,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    // Navigate to terms and conditions page
+                                  },
+                              ),
+                              TextSpan(
+                                text: ' & ',
+                              ),
+                              TextSpan(
+                                text: 'privacy policy',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  decoration: TextDecoration.underline,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    // Navigate to privacy policy page
+                                  },
+                              ),
+                              TextSpan(
+                                text: ' stated.',
+                              ),
+                            ],
+                          ),
+                        )
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: isChecked2,
+                        onChanged: (value) {
+                          setState(() {
+                            isChecked2 = value;
+                          });
+                        },
+                        checkColor: Colors.black,
+                        activeColor: Colors.grey[400],
+                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        visualDensity: VisualDensity.compact,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'Subscribe to our latest newsfeed for upcoming updates.',
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 6),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green[900],
+                        minimumSize: Size(320, 40)
+                    ),
+                    onPressed: (){
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const NavigationMenu()),
+                      );
+                    },
+                    child: Text('Register'),
                   ),
                 ],
               ),
