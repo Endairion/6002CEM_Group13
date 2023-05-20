3bf9c590b50f4f77af35cfc782f64bae49153906
Jia Wei <jiawei392@gmail.com>
2023-05-20 16:09:05 +0800

    Add onTap to available list card

--- lib/SearchAvailableTrips.dart
+++ lib/SearchAvailableTrips.dart
@@ -355,143 +355,148 @@ Widget TripCard(List<Trip> trip, int index) {
 
   return Padding(
     padding: const EdgeInsets.only(bottom: 12),
-    child: Container(
-      height: 120,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(20))),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(12, 14, 12, 12),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 4),
-                      child: Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          borderRadius: new BorderRadius.circular(30.0),
-                          color: Colors.limeAccent[700],
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'S',
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
+    child: GestureDetector(
+      onTap: (){
+        print ("Container $index clicked");
+      },
+      child: Container(
+        height: 120,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(12, 14, 12, 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 4),
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            borderRadius: new BorderRadius.circular(30.0),
+                            color: Colors.limeAccent[700],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'S',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Text(
-                      startLocation,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black87,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 4),
-                      child: Container(
-                        width: 20,
-                        height: 20,
-                        decoration: BoxDecoration(
-                          borderRadius: new BorderRadius.circular(30.0),
-                          color: Colors.green[900],
+                      Text(
+                        startLocation,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black87,
                         ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'D',
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 4),
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          decoration: BoxDecoration(
+                            borderRadius: new BorderRadius.circular(30.0),
+                            color: Colors.green[900],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'D',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Text(
-                      destination,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black87,
+                      Text(
+                        destination,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black87,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 4,
-                    ),
-                    Text(
-                      'Date: ' + trip[index].date,
+                    ],
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        'Date: ' + trip[index].date,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        'Time: ' + trip[index].time,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4),
+                    child: Text(
+                      'Available Seats: ' + trip[index].seats.toString(),
                       style: TextStyle(
                         fontSize: 14,
-                        color: Colors.black87,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red[300],
                       ),
-                    ),
-                    SizedBox(
-                      width: 12,
-                    ),
-                    Text(
-                      'Time: ' + trip[index].time,
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.black87,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 4),
-                  child: Text(
-                    'Available Seats: ' + trip[index].seats.toString(),
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red[300],
                     ),
                   ),
-                ),
-              ],
-            ),
-            Icon(
-              Icons.navigate_next,
-              size: 45,
-              color: Colors.green[900],
-            ),
-          ],
+                ],
+              ),
+              Icon(
+                Icons.navigate_next,
+                size: 45,
+                color: Colors.green[900],
+              ),
+            ],
+          ),
         ),
       ),
     ),
