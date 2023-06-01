0114fa24e254c53dc9c0103ec2c5b30fdf7778e9
Jia Wei <jiawei392@gmail.com>
2023-06-01 23:48:51 +0800

    Create trip details view model

---
+++ lib/viewmodels/trip_details_viewmodel.dart
@@ -0,0 +1,19 @@
+import 'package:mobile_app_development_cw2/locator.dart';
+import 'package:mobile_app_development_cw2/models/trip_model.dart';
+import 'package:mobile_app_development_cw2/services/firebase_service.dart';
+import 'package:mobile_app_development_cw2/utils/validators.dart';
+import 'package:mobile_app_development_cw2/viewmodels/base_viewmodel.dart';
+
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class PlanTripViewModel extends BaseViewModel {
+
+  // Services
+  final FirebaseService _firebaseService = locator<FirebaseService>();
+
+
+}
