cd5157a63d1932bbc383d9b34f8c42f8b31f2619
Endairion <86313950+Endairion@users.noreply.github.com>
2023-06-17 18:59:10 +0800

    Hot Fix #7

--- lib/viewmodels/edit_profile_viewmodel.dart
+++ lib/viewmodels/edit_profile_viewmodel.dart
@@ -1,29 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app_development_cw2/services/firebase_service.dart';
+import 'package:mobile_app_development_cw2/utils/validators.dart';
 import 'package:mobile_app_development_cw2/viewmodels/base_viewmodel.dart';
 import 'package:mobile_app_development_cw2/locator.dart';
 
 class EditProfileViewModel extends BaseViewModel{
   final FirebaseService _service = locator<FirebaseService>();
-  late TextEditingController nameController = TextEditingController();
-  late TextEditingController emailController = TextEditingController();
-  late TextEditingController dobController = TextEditingController();
-  late TextEditingController icNoController = TextEditingController();
-  late TextEditingController contactController = TextEditingController();
+  late TextEditingController _nameController = TextEditingController();
+  late TextEditingController _emailController = TextEditingController();
+  late TextEditingController _dobController = TextEditingController();
+  late TextEditingController _icNoController = TextEditingController();
+  late TextEditingController _contactController= TextEditingController();
+
+  TextEditingController get nameController => _nameController;
+  TextEditingController get emailController => _emailController;
+  TextEditingController get dobController => _dobController;
+  TextEditingController get icNoController => _icNoController;
+  TextEditingController get contactController => _contactController;
+
+  String? Function(String? name) get nameValidator => Validators.nameValidator;
+  String? Function(String? email) get emailValidator => Validators.emailValidator;
+  String? Function(String? dob) get dobValidator => Validators.dobValidator;
+  String? Function(String? icNo) get icNoValidator => Validators.icNoValidator;
+  String? Function(String? contact) get contactValidator => Validators.contactValidator;
 
 
   void onModelReady(){
     fetchUserProfile();
+    notifyListeners();
   }
 
   void fetchUserProfile() async {
     try {
       var userProfile = await _service.fetchUserProfile();
-      nameController = TextEditingController(text: userProfile['name'] as String);
-      emailController = TextEditingController(text: userProfile['email'] as String);
-      dobController = TextEditingController(text: userProfile['dob'] as String);
-      icNoController = TextEditingController(text: userProfile['ic_no'] as String);
-      contactController = TextEditingController(text: userProfile['contact'] as String);
+      _nameController = TextEditingController(text: userProfile['name'] as String);
+      _emailController = TextEditingController(text: userProfile['email'] as String);
+      _dobController = TextEditingController(text: userProfile['dob'] as String);
+      _icNoController = TextEditingController(text: userProfile['ic_no'] as String);
+      _contactController = TextEditingController(text: userProfile['contact'] as String);
       notifyListeners();
     } catch (e) {
       print('Error fetching user profile: $e');
@@ -54,7 +68,7 @@ class EditProfileViewModel extends BaseViewModel{
       // fetchUserProfile();
       // notifyListeners(); // Notify listeners if necessary
     } catch (e) {
-      print('Error updating user profile: $e');
+      throw 'Error updating user profile: $e';
     }
   }
 
